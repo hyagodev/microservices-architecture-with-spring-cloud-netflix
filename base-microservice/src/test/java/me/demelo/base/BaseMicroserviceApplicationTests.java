a652b6a34ab6e60d34d1f53c4e797a198e2eec50
Hyago de Melo Rolindo <hyago@demelo.me>
2024-01-06 22:43:48 -0300

    criação do projeto base

---
+++ base-microservice/src/test/java/me/demelo/base/BaseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.demelo.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
